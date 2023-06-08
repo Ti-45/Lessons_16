9e346479312067129ade65fe517b2d60d04f6159
Ti45A <StepanyanAzat@gmail.com>
2023-06-08 05:52:24 +0400

    python

--- app-code/port_48888.py
+++ app-code/port_48888.py
@@ -1,41 +1,15 @@
-import socket
 import http.server
 import socketserver
 
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"port request succeeded")
+
 PORT = 48888
 
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("The server is running on a port", PORT)
     httpd.serve_forever()
-
-def start_server():
-    # Создаем сокет
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Привязываем сокет к адресу и порту
-    s.bind(('localhost', 48888))
-
-    # Слушаем порт
-    s.listen(1)
-    print('Server started, listening on port 48888...')
-
-    while True:
-        # Принимаем входящее соединение
-        conn, addr = s.accept()
-        print('Connected by', addr)
-        
-        # Отправляем сообщение клиенту
-        message = b'48888 porti harcman patasxan@ '
-        conn.sendall(message)       
-        
-        # Записываем сообщение в файл
-        with open('output.txt', 'wb') as f:
-            f.write(message)
-
-        # Закрываем соединение
-        conn.close()
-
-if __name__ == '__main__':
-    start_server()
