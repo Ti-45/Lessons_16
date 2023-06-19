16061980c19d9f86b2bdde5dadfc51254012179d
Ti-45 <stepanyanazat@gmail.com>
2023-06-19 04:08:35 +0400

    fix

--- app-code/portforward.py
+++ app-code/portforward.py
@@ -7,7 +7,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        trainer_name = os.environ.get('TRAINER_NAME','Karen Kostanyan')
+        trainer_name = os.environ.get('TRAINER_NAME','Undefined')
         port = int(os.environ.get('PORT', 48888))
         response_text = f"Hello {trainer_name}. This is my exam project text"
         self.wfile.write(response_text.encode())
