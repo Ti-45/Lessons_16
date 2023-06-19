b6b3564dc0a0a8720c1e84163fcf51e7bf37ad70
Ti-45 <stepanyanazat@gmail.com>
2023-06-19 22:07:29 +0400

    fix

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,11 @@ FROM ubuntu:22.04
 # Sets the working directory inside the container to /app-code
 WORKDIR /app-code
 
-# Updates the package lists for the Ubuntu package manager (apt), upgrades the installed packages to their latest versions, and installs Python 3
-RUN apt update && apt upgrade -y && apt install python3  python3-pip python3-flask -y
+# Updates the package lists for the Ubuntu package manager (apt), upgrades the installed packages to their latest versions, and installs Python3
+RUN apt update && apt upgrade -y && apt install python3  -y
 
 # Copies the contents of the local ./app-code/ directory into the /app-code/ directory in the container
 COPY --chown=1000:1000  ./app-code/ /app-code/
 
-# Specifies the default command to be executed when a container is created from this image. It runs the Python script port_48888.py using the python3 interpreter.
+# Specifies the default command to be executed when a container is created from this image. It runs the Python script appreadport.py using the python3 interpreter.
 CMD [ "python3", "appreadport.py" ]
--- app-code/appreadport.py
+++ app-code/appreadport.py
@@ -1,12 +1,22 @@
+import http.server
 import os
-from flask import Flask, app
+import socketserver
 
-app = Flask(__name__)
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        trainer_name = os.environ.get('TRAINER_NAME','Undefined')
+        port = int(os.environ.get('PORT', 48888))
+        response_text = f"Hello {trainer_name}. This is my exam project text"
+        self.wfile.write(response_text.encode())
 
-@app.route('/')
-def hello_world():
-    trainer_name = os.environ.get('TRAINER_NAME', 'Undefined')
-    return f'Hello {trainer_name}! This is my exam project.'
+def run_server(port):
+    with socketserver.TCPServer(("", port), MyHandler) as httpd:
+        print("The server is running on port", port)
+        httpd.serve_forever()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 48888))
+    run_server(port)
